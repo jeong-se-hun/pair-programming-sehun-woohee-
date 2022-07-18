dfc5e6d0e7a741b6e56593bdc62a9ad86466faac
jeong-se-hun <tpgns933@gmail.com>
2022-07-18 17:58:43 +0900

    palindrome pull 하기전 커밋

--- 1.Scrolling/app.js
+++ 1.Scrolling/app.js
@@ -1,18 +1,11 @@
 (() => {
-  const $scrollIcon = document.querySelector(".scroll-icon");
+  const $scrollIcon = document.querySelector('.scroll-icon');
   window.addEventListener(
-    "scroll",
-    _.throttle(
-      () =>
-        ($scrollIcon.style.display =
-          window.pageYOffset > 300 ? "block" : "none"),
-      500
-    )
+    'scroll',
+    _.throttle(() => ($scrollIcon.style.display = window.pageYOffset > 300 ? 'block' : 'none'), 500)
   );
 
-  $scrollIcon.addEventListener("click", () =>
-    window.scrollTo({ top: 0, behavior: "smooth" })
-  );
+  $scrollIcon.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
 })();
 
 // TODO: 스크롤 올라가는 것
