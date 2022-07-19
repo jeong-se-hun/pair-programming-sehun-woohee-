037d4852e72c080e8592b89d111f0935d235f032
jeong-se-hun <tpgns933@gmail.com>
2022-07-19 14:51:58 +0900

    stopWatch 리팩토링 1차

--- 6.stopWatch/app.js
+++ 6.stopWatch/app.js
@@ -1,16 +1,14 @@
-const $stopwatch = document.querySelector('.stopwatch');
 const $laps = document.querySelector('.laps');
+const $resetLapButton = document.querySelectorAll('.control')[1];
 
 const stopwatch = {
   defaultTime: '00:00:00',
   startTime: 0,
   elapsedTime: 0,
   interval: null,
-
   print(text) {
     document.querySelector('.display').innerHTML = text;
   },
-
   addZero(number) {
     if (number < 10) {
       return '0' + number;
@@ -20,34 +18,27 @@ const stopwatch = {
     }
     return number;
   },
-
   timeToString(time) {
     const date = new Date(time);
     const minutes = date.getUTCMinutes();
     const seconds = date.getUTCSeconds();
     const millisecond = date.getMilliseconds();
-    return `${this.addZero(minutes)}:${this.addZero(seconds)}:${this.addZero(
-      millisecond
-    )}`;
+    return `${this.addZero(minutes)}:${this.addZero(seconds)}:${this.addZero(millisecond)}`;
   },
-
   startTimer() {
     this.elapsedTime = Date.now() - this.startTime;
     const time = this.timeToString(this.elapsedTime);
     this.print(time);
   },
-
   start() {
     clearInterval(this.interval);
     this.startTime = Date.now() - this.elapsedTime;
     this.interval = setInterval(this.startTimer.bind(this, 10));
   },
-
   stop() {
     console.log(this.interval);
     clearInterval(this.interval);
   },
-
   reset() {
     clearInterval(this.interval);
     this.print(this.defaultTime);
@@ -55,37 +46,35 @@ const stopwatch = {
     this.elapsedTime = 0;
     this.interval = null;
   },
-
   laps() {
-    const lapsId = document.createElement('div');
-    lapsId.textContent = document.querySelector('.laps').children.length / 2;
-    document.querySelector('.laps').appendChild(lapsId);
-    const lapsValue = document.createElement('div');
+    const lapsId = document.createElement('span');
+    lapsId.textContent = $laps.children.length / 2;
+    $laps.appendChild(lapsId);
+    const lapsValue = document.createElement('span');
     lapsValue.textContent = this.timeToString(this.elapsedTime);
-    document.querySelector('.laps').appendChild(lapsValue);
+    $laps.appendChild(lapsValue);
   },
 };
-$stopwatch.addEventListener('click', (e) => {
+document.querySelector('.stopwatch').addEventListener('click', e => {
   if (e.target.textContent === 'Start') {
-    e.target.nextElementSibling.textContent = 'Lap';
-    e.target.nextElementSibling.disabled = false;
+    $resetLapButton.textContent = 'Lap';
+    $resetLapButton.disabled = false;
     e.target.textContent = 'Stop';
     stopwatch.start();
   } else if (e.target.textContent === 'Stop') {
-    e.target.nextElementSibling.textContent = 'Reset';
+    $resetLapButton.textContent = 'Reset';
     e.target.textContent = 'Start';
     stopwatch.stop();
   }
-
   if (e.target.textContent === 'Reset') {
     stopwatch.reset();
     $laps.style.display = 'none';
     e.target.disabled = true;
-    document.querySelector('.laps').innerHTML = `
+    $laps.innerHTML = `
     <div class="lap-title">Laps</div>
     <div class="lap-title">Time</div>`;
   }
-
+  // TODO: innerHTML 초기화가 답인가???
   if (e.target.textContent === 'Lap') {
     $laps.style.display = 'grid';
     stopwatch.laps();
