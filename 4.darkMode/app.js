a2779ed715651d277ee94611b4edfd37c5051442
jeong-se-hun <tpgns933@gmail.com>
2022-07-19 12:53:18 +0900

    darkmode 수정

--- 4.darkMode/app.js
+++ 4.darkMode/app.js
@@ -7,7 +7,11 @@
     } else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
       $body.classList.add('dark');
     }
-    $body.classList.remove('hide');
+
+    // TODO: 이거를 어찌할까 트렌지션 엔드??
+    setTimeout(() => {
+      $body.classList.remove('hide');
+    }, 200);
   });
 
   document.querySelector('.toggle-button').addEventListener('click', () => {
