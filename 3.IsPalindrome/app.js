31ef23db74c02151970e1f0c539cc3ef5def0bc2
jeong-se-hun <tpgns933@gmail.com>
2022-07-19 19:48:56 +0900

    일부 수정(리펙토링 진행중)

--- 3.IsPalindrome/app.js
+++ 3.IsPalindrome/app.js
@@ -3,14 +3,15 @@
 
   $palindromeForm.addEventListener('submit', e => {
     e.preventDefault();
+
     const palindromeInputValue = document.querySelector('.palindrome-input').value;
+
     document.querySelector('.palindrome-result').innerHTML = checkPalindrome(palindromeInputValue)
       ? `"${palindromeInputValue}" is a palindrome`
       : `"${palindromeInputValue}" is not a palindrome`;
     palindromeInputValue = '';
   });
-  // TODO: value까지 변수로 묶는것
-  // TODO: eslint 따움표
+
   const checkPalindrome = str => {
     const string = str.toUpperCase().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/ ]/gim, '');
     return !!string && string.split('').reverse().join('') === string;
--- 8.toster/app.js
+++ 8.toster/app.js
@@ -2,17 +2,14 @@ const $showSuccess = document.querySelector('.show-success');
 const $showError = document.querySelector('.show-error');
 const $showWarning = document.querySelector('.show-warning');
 const $body = document.querySelector('body');
-const messageContainer = document.createElement('div');
-$body.appendChild(messageContainer);
 
-const showMessage = (element) => {
+const showMessage = element => {
   const state = element.textContent.toLowerCase();
-  const messageBox = document.createElement('div');
-  messageBox.innerHTML = `
-  <div class="toast ${state}"">
-  <h4 class="toast-title">${
-    state === 'success' ? 'Well done!' : 'Check it out!'
-  }</h4>
+  const $toast = document.createElement('div');
+  $toast.classList.add('toast', state);
+  $toast.style.bottom = 0;
+  $toast.innerHTML = `
+  <h4 class="toast-title">${state === 'success' ? 'Well done!' : 'Check it out!'}</h4>
   <div class="toast-message">
   <svg width="24" height="24">
   <use xlink:href="#${state}" />
@@ -20,18 +17,33 @@ const showMessage = (element) => {
   <p>this is a ${state} alert</p>
   </div>
   <a class="toast-close">&times;</a>
-  </div>`;
-  messageContainer.appendChild(messageBox);
+ `;
+  $body.appendChild($toast);
+
+  $body.querySelectorAll('.toast').forEach((toast, i, arr) => {
+    if (i === arr.length - 1) return;
+    const bottom = toast.style.bottom.replace(/[^0-9]/g, '');
+    toast.style.bottom = `${+bottom + 100}px`;
+  });
+
   setTimeout(() => {
-    if ($body.querySelector('.toast') !== null) $body.removeChild(messageBox);
-  }, 3000);
+    if ($body.querySelector('.toast') !== null) $body.removeChild($toast);
+  }, 10000);
 };
 
-$showSuccess.addEventListener('click', (e) => {
+$showSuccess.addEventListener('click', e => {
+  showMessage(e.target);
+});
+$showError.addEventListener('click', e => {
+  showMessage(e.target);
+});
+$showWarning.addEventListener('click', e => {
   showMessage(e.target);
 });
 
-$body.addEventListener('click', (e) => {
-  if (e.target.classList.contains('toast-close'))
-    $body.removeChild(e.target.parentNode.parentNode.parentNode);
+$body.addEventListener('click', e => {
+  if (e.target.classList.contains('toast-close')) {
+    $body.removeChild(e.target.parentNode);
+    console.log(e.target.parentNode);
+  }
 });
