ee0e2bf9ca96a81264649199098225019a06be0f
jeong-se-hun <tpgns933@gmail.com>
2022-07-20 21:10:25 +0900

    carousel 진행중

--- 10.CarouselSlider/app.js
+++ 10.CarouselSlider/app.js
@@ -1,3 +1,8 @@
+// const $carouselControl = document.querySelector('.carousel-control');
+const $carousel = document.querySelector('.carousel');
+
+let width = 0;
+
 const carousel = ($container, images) => {
   const img = new Image();
   img.src = '' + images[0];
@@ -5,21 +10,49 @@ const carousel = ($container, images) => {
   <img src="${images[images.length - 1]}">
   ${images.map(img => `<img src="${img}">`).join('')}
   <img src="${images[0]}">
-      </div>
-      <button class="carousel-control prev">&laquo;</button>
-      <button class="carousel-control next">&raquo;</button>
-   </img>`;
-  $container.style.width = `${img.width}px`;
-  $container.style.opacity = 1;
-
-  //  $container.style.width = image
+  </div>
+  <button class="carousel-control prev">&laquo;</button>
+  <button class="carousel-control next">&raquo;</button>
+  </img>`;
 };
-carousel(document.querySelector('.carousel'), [
-  './movies/movie-1.jpg',
-  './movies/movie-2.jpg',
-  './movies/movie-3.jpg',
-  './movies/movie-4.jpg',
-]);
-document.querySelector('.carousel-control').addEventListener('click', e => {});
-const img = document.querySelector('img');
-console.dir(img.offsetWidth);
+
+carousel($carousel, ['./movies/movie-1.jpg', './movies/movie-2.jpg', './movies/movie-3.jpg', './movies/movie-4.jpg']);
+const setFrame = () => {
+  $carousel.firstElementChild.style.setProperty('--currentSlide', 1);
+};
+
+window.addEventListener('load', () => {
+  width = document.querySelector('img').getBoundingClientRect().width;
+  $carousel.style.width = `${width}px`;
+  setFrame();
+  $carousel.style.opacity = 1;
+});
+
+$carousel.addEventListener('click', e => {
+  $carousel.firstElementChild.style.setProperty('--duration', 1000);
+
+  if (e.target.classList.contains('next')) {
+    $carousel.firstElementChild.style.setProperty(
+      '--currentSlide',
+      +$carousel.firstElementChild.style.getPropertyValue('--currentSlide') + 1
+    );
+
+    if ($carousel.firstElementChild.style.getPropertyValue('--currentSlide') === '5') {
+      setTimeout(() => {
+        $carousel.firstElementChild.style.setProperty('--duration', 0);
+        $carousel.firstElementChild.style.setProperty('--currentSlide', 1);
+
+        console.log($carousel.firstElementChild.style.getPropertyValue('--currentSlide'));
+      }, 1000);
+    }
+
+    console.log($carousel.firstElementChild.style.getPropertyValue('--currentSlide'));
+  }
+
+  if (e.target.classList.contains('prev')) {
+    $carousel.firstElementChild.style.setProperty(
+      '--currentSlide',
+      +$carousel.firstElementChild.style.getPropertyValue('--currentSlide') - 1
+    );
+  }
+});
