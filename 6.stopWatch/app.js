a80a4fa5f15f5739e10f522053507c0558f26e5e
woohee Byun <cay107@naver.com>
2022-07-24 13:14:00 +0900

    before refectoring

--- 6.stopWatch/app.js
+++ 6.stopWatch/app.js
@@ -1,6 +1,107 @@
+// (() => {
+//   const $laps = document.querySelector('.laps');
+//   const $resetLapBtn = document.querySelectorAll('.control')[1];
+
+//   const stopwatch = {
+//     defaultTime: '00:00:00',
+//     startTime: 0,
+//     elapsedTime: 0,
+//     interval: null,
+
+//     print(text) {
+//       document.querySelector('.display').innerHTML = text;
+//     },
+
+//     addZero(number) {
+//       if (number < 10) {
+//         return '0' + number;
+//       }
+//       if (number > 99) {
+//         return number.toString().slice(0, -1);
+//       }
+//       return number;
+//     },
+
+//     timeToString(time) {
+//       const date = new Date(time);
+//       const minutes = date.getUTCMinutes();
+//       const seconds = date.getUTCSeconds();
+//       const millisecond = date.getMilliseconds();
+//       return `${this.addZero(minutes)}:${this.addZero(seconds)}:${this.addZero(millisecond)}`;
+//     },
+
+//     startTimer() {
+//       this.elapsedTime = Date.now() - this.startTime;
+//       const time = this.timeToString(this.elapsedTime);
+//       this.print(time);
+//     },
+
+//     start() {
+//       clearInterval(this.interval);
+//       this.startTime = Date.now() - this.elapsedTime;
+//       this.interval = setInterval(this.startTimer.bind(this, 10));
+//     },
+
+//     stop() {
+//       clearInterval(this.interval);
+//     },
+
+//     reset() {
+//       clearInterval(this.interval);
+//       this.print(this.defaultTime);
+//       this.startTime = 0;
+//       this.elapsedTime = 0;
+//       this.interval = null;
+//     },
+
+//     laps() {
+//       const $lapsId = document.createElement('span');
+//       const $lapsValue = document.createElement('span');
+//       $lapsId.textContent = $laps.children.length / 2;
+//       $lapsValue.textContent = this.timeToString(this.elapsedTime);
+//       $laps.append($lapsId);
+//       $laps.appendChild($lapsValue);
+//     },
+//   };
+
+//   document.querySelector('.stopwatch').addEventListener('click', e => {
+//     if (e.target.textContent === 'Start') {
+//       $resetLapBtn.disabled = false;
+//       $resetLapBtn.textContent = 'Lap';
+
+//       e.target.textContent = 'Stop';
+
+//       stopwatch.start();
+//     } else if (e.target.textContent === 'Stop') {
+//       $resetLapBtn.textContent = 'Reset';
+
+//       e.target.textContent = 'Start';
+
+//       stopwatch.stop();
+//     }
+
+//     if (e.target.textContent === 'Reset') {
+//       stopwatch.reset();
+
+//       e.target.disabled = true;
+//       $laps.style.display = 'none';
+//       $laps.innerHTML = `
+//       <div class="lap-title">Laps</div>
+//       <div class="lap-title">Time</div>`;
+//     }
+
+//     if (e.target.textContent === 'Lap') {
+//       $laps.style.display = 'grid';
+
+//       stopwatch.laps();
+//     }
+
+//     // 함수형 고려??
+//   });
+// })();
 (() => {
   const $laps = document.querySelector('.laps');
-  const $resetLapButton = document.querySelectorAll('.control')[1];
+  const [$startStopBtn, $resetLapBtn] = document.querySelectorAll('.control');
 
   const stopwatch = {
     defaultTime: '00:00:00',
@@ -9,7 +110,7 @@
     interval: null,
 
     print(text) {
-      document.querySelector('.display').innerHTML = text;
+      document.querySelector('.display').textContent = text;
     },
 
     addZero(number) {
@@ -40,10 +141,16 @@
       clearInterval(this.interval);
       this.startTime = Date.now() - this.elapsedTime;
       this.interval = setInterval(this.startTimer.bind(this, 10));
+      $resetLapBtn.disabled = false;
+      $resetLapBtn.textContent = 'Lap';
+      $startStopBtn.textContent = 'Stop';
     },
 
     stop() {
       clearInterval(this.interval);
+      $resetLapBtn.textContent = 'Reset';
+
+      $startStopBtn.textContent = 'Start';
     },
 
     reset() {
@@ -52,44 +159,39 @@
       this.startTime = 0;
       this.elapsedTime = 0;
       this.interval = null;
+      $resetLapBtn.disabled = true;
+      $laps.style.display = 'none';
+      $laps.innerHTML = `
+      <div class="lap-title">Laps</div>
+      <div class="lap-title">Time</div>`;
     },
 
     laps() {
-      const lapsId = document.createElement('span');
-      lapsId.textContent = $laps.children.length / 2;
-      $laps.appendChild(lapsId);
-      const lapsValue = document.createElement('span');
-      lapsValue.textContent = this.timeToString(this.elapsedTime);
-      $laps.appendChild(lapsValue);
+      const $lapsId = document.createElement('span');
+      const $lapsValue = document.createElement('span');
+      $lapsId.textContent = $laps.children.length / 2;
+      $lapsValue.textContent = this.timeToString(this.elapsedTime);
+      $laps.append($lapsId);
+      $laps.appendChild($lapsValue);
+      $laps.style.display = 'grid';
     },
   };
 
   document.querySelector('.stopwatch').addEventListener('click', e => {
-    if (e.target.textContent === 'Start') {
-      $resetLapButton.textContent = 'Lap';
-      $resetLapButton.disabled = false;
-      e.target.textContent = 'Stop';
-      stopwatch.start();
-    } else if (e.target.textContent === 'Stop') {
-      $resetLapButton.textContent = 'Reset';
-      e.target.textContent = 'Start';
-      stopwatch.stop();
-    }
-
-    if (e.target.textContent === 'Reset') {
-      stopwatch.reset();
-      $laps.style.display = 'none';
-      e.target.disabled = true;
-      $laps.innerHTML = `
-    <div class="lap-title">Laps</div>
-    <div class="lap-title">Time</div>`;
-    }
-
-    if (e.target.textContent === 'Lap') {
-      $laps.style.display = 'grid';
-      stopwatch.laps();
-    }
-
-    // 함수형 고려??
+    if (e.target.textContent === 'Start') return stopwatch.start();
+    if (e.target.textContent === 'Reset') return stopwatch.reset();
+    if (e.target.textContent === 'Stop') return stopwatch.stop();
+    if (e.target.textContent === 'Lap') return stopwatch.laps();
   });
 })();
+// 익스플로어에서 appendChild 지원
+// 여러번 호출시 reflow가 많이 일어남
+
+// const $lapsFragment = document.createDocumentFragment();
+// const $lapsId = document.createElement('span');
+// const $lapsValue = document.createElement('span');
+// $lapsId.textContent = $laps.children.length / 2;
+// $lapsValue.textContent = this.timeToString(this.elapsedTime);
+// $lapsFragment.appendChild($lapsId);
+// $lapsFragment.appendChild($lapsValue);
+// $laps.append($lapsFragment);
